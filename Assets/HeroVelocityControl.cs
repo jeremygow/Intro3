3020a4d01cf6c5ab3509ce9ffd4d12df155a9d0f
Jeremy <jeremy.gow@gmail.com>
2015-01-30 15:42:04 +0000

    Some stuff to start with

---
+++ Assets/HeroVelocityControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroVelocityControl : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		
+		float move = Input.GetAxis ("Horizontal");
+		
+		float up = Input.GetAxis ("Vertical");
+		
+		if (up > 0) {
+			rigidbody2D.velocity = new Vector2 (move * maxSpeed, up * maxSpeed);
+		} else {
+			rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+		}
+		
+	}
+}
